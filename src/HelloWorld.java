0d9b324b1611f9d3a204bcb74f41eab158700516
joshkgcc <killamjs21@gcc.edu>
2023-03-01 13:42:21 -0500

    Is this working?

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello " + name);
     }
 }
